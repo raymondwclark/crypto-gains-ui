4ca40dee8f3d15c27d5e345f41060beb14168fac
Raymond Clark <raymondclark@Raymonds-MacBook-Pro.local>
2021-05-25 12:27:18 -0400

    added pull to refresh

--- src/components/DashboardComponent.js
+++ src/components/DashboardComponent.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import BalanceComponent from './BalanceComponent';
 import axios from 'axios';
 import moment from 'moment';
+import PullToRefresh from 'react-simple-pull-to-refresh';
 
 const DashboardComponent = () => {
     const [balances, setBalances] = useState({});
@@ -27,21 +28,23 @@ const DashboardComponent = () => {
     }
 
     return (
-        <div style={{marginTop: '120px'}}>
-            <BalanceComponent balance={balances?.binance?.current} last={balances?.binance?.last} title='binance'></BalanceComponent>
-            <BalanceComponent balance={balances?.safemoon?.current} last={balances?.safemoon?.last} title='safemoon'></BalanceComponent>
-            <BalanceComponent balance={balances?.bonfire?.current} last={balances?.bonfire?.last} title='bonfire'></BalanceComponent>
-            <BalanceComponent balance={balances?.total?.current} last={balances?.total?.last} title='total'></BalanceComponent>
-            <label style={{textAlign: 'center', marginTop: '40px', color: 'rgb(120, 120, 120)', fontSize: '12px'}}>
-                Since {moment(lastPulled).format('MMMM Do YYYY, h:mm:ss a') || 'now'}
-            </label>
-            <label style={{textAlign: 'center', color: 'rgb(120, 120, 120)', fontSize: '10px'}}>
-                (Resets every 24 hrs)
-            </label>
-            <label style={{textAlign: 'center', color: 'rgb(120, 120, 120)', fontSize: '10px'}}>
-                Last refreshed {moment(new Date()).format('MMMM Do YYYY, h:mm:ss a')}
-            </label>
-        </div>
+            <div style={{marginTop: '100px'}}>
+                <PullToRefresh onRefresh={getBalances} pullDownThreshold='160' maxPullDownDistance='160' style={{marginTop: '120px'}}>
+                    <BalanceComponent balance={balances?.binance?.current} last={balances?.binance?.last} title='binance'></BalanceComponent>
+                    <BalanceComponent balance={balances?.safemoon?.current} last={balances?.safemoon?.last} title='safemoon'></BalanceComponent>
+                    <BalanceComponent balance={balances?.bonfire?.current} last={balances?.bonfire?.last} title='bonfire'></BalanceComponent>
+                    <BalanceComponent balance={balances?.total?.current} last={balances?.total?.last} title='total'></BalanceComponent>
+                    <label style={{textAlign: 'center', marginTop: '40px', color: 'rgb(120, 120, 120)', fontSize: '12px'}}>
+                        Since {moment(lastPulled).format('MMMM Do YYYY, h:mm:ss a') || 'now'}
+                    </label>
+                    <label style={{textAlign: 'center', color: 'rgb(120, 120, 120)', fontSize: '10px'}}>
+                        (Resets every 24 hrs)
+                    </label>
+                    <label style={{textAlign: 'center', color: 'rgb(120, 120, 120)', fontSize: '10px'}}>
+                        Last refreshed {moment(new Date()).format('MMMM Do YYYY, h:mm:ss a')}
+                    </label>
+                </PullToRefresh>
+            </div>
     )
 }
 
